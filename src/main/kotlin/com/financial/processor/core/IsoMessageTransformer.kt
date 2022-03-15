c6fe47888439b41450e0c77a7eb6be324ff47f07
David Sanford <dsanfordseu@gmail.com>
2022-03-15 00:19:11 -0300

    Add debug logging for all components + parameterize header length

--- src/main/kotlin/com/financial/processor/core/IsoMessageTransformer.kt
+++ src/main/kotlin/com/financial/processor/core/IsoMessageTransformer.kt
@@ -3,11 +3,21 @@ package com.financial.processor.core
 import com.financial.processor.core.log.IsoLogger
 import org.apache.commons.logging.LogFactory
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
+import javax.annotation.PostConstruct
 
 
 @Component("financial-processor.iso-message-transformer")
 class IsoMessageTransformer @Autowired constructor(private val messageFactory: IsoMessageFactory, private val isoLogger: IsoLogger) {
+
+    @Value("\${financial-processor.iso8583.header-lenght}")
+    val headerLength = 0
+
+    @PostConstruct
+    fun logHeaderLength() {
+        print("Iso header length: $headerLength\n")
+    }
 
     fun transformToString(message: FinancialMessage): String {
         val isoMessageString = String(message.fields.writeData())
@@ -17,7 +27,7 @@ class IsoMessageTransformer @Autowired constructor(private val messageFactory: I
 
     fun transformFromString(message: String): FinancialMessage {
         log.debug("Incoming iso message: $message")
-        return messageFactory.buildIsoMessage(message, 0, FinancialMessage.Type.INCOMING) //TODO: parameterize header
+        return messageFactory.buildIsoMessage(message, headerLength, FinancialMessage.Type.INCOMING) //TODO: parameterize header
     }
 
     companion object {
